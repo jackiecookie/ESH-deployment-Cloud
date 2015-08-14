62929dd3381e585964aadabf49a1a828ccf99458
wspandihai <2711002357@qq.com>
2015-08-14 17:32:11 +0800

    修改

--- File.js
+++ File.js
@@ -4,8 +4,22 @@ var pathModule = require('path');
 var exists = require('fs').existsSync;
 
 
-var File = function(p, dirname) {
+// var File = function(p, dirname) {
 
+// 	if (!dirname) dirname = process.cwd();
+// 	var extname = pathModule.extname(p);
+// 	p += (extname ? '' : '.js');
+// 	var rePath = pathModule.relative(dirname, p);
+// 	var abPath = pathModule.resolve(dirname, rePath);
+// 	this.path = abPath;
+// 	this.rePath = rePath;
+// 	this.extname = extname || '.js';
+// 	this.exists = exists(this.path);
+// }
+
+
+
+var File = function(p, dirname) {
 	if (!dirname) dirname = process.cwd();
 	var extname = pathModule.extname(p);
 	p += (extname ? '' : '.js');
@@ -14,8 +28,14 @@ var File = function(p, dirname) {
 	this.path = abPath;
 	this.rePath = rePath;
 	this.extname = extname || '.js';
-	this.exists = exists(this.path);
+
+	Object.defineProperty(this, "exists", {
+		get: function() {
+			return exists(abPath);
+		}
+	});
 }
+
 
 
 module.exports = File;
